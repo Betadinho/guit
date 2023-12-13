ff0d7e35137bf684c529e5fbdc186e80a2cfb0d5
alphamario.prechtl <prechtl@billiger-mietwagen.de>
2023-12-13 16:39:19 +0100

    [CHORE] Code cleanup

--- js/fretboard.js
+++ js/fretboard.js
@@ -12,25 +12,25 @@ var notes = {
     d: ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D'],
     g: ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G'],
     b: ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-}
+};
 
 for (let i = 0; i < notes.e.length; i++) {
-    document.getElementById('mask_low_e_list').insertAdjacentHTML('beforeend', '<li note=' + notes.e[i] + '>' + notes.e[i] + '</li>')
-    document.getElementById('mask_a_list').insertAdjacentHTML('beforeend', '<li note=' + notes.a[i] + '>' + notes.a[i] + '</li>')
-    document.getElementById('mask_d_list').insertAdjacentHTML('beforeend', '<li note=' + notes.d[i] + '>' + notes.d[i] + '</li>')
-    document.getElementById('mask_g_list').insertAdjacentHTML('beforeend', '<li note=' + notes.g[i] + '>' + notes.g[i] + '</li>')
-    document.getElementById('mask_b_list').insertAdjacentHTML('beforeend', '<li note=' + notes.b[i] + '>' + notes.b[i] + '</li>')
-    document.getElementById('mask_high_e_list').insertAdjacentHTML('beforeend', '<li note=' + notes.e[i] + '>' + notes.e[i] + '</li>')
-}
+    document.getElementById('mask_low_e_list').insertAdjacentHTML('beforeend', '<li note=' + notes.e[i] + '>' + notes.e[i] + '</li>');
+    document.getElementById('mask_a_list').insertAdjacentHTML('beforeend', '<li note=' + notes.a[i] + '>' + notes.a[i] + '</li>');
+    document.getElementById('mask_d_list').insertAdjacentHTML('beforeend', '<li note=' + notes.d[i] + '>' + notes.d[i] + '</li>');
+    document.getElementById('mask_g_list').insertAdjacentHTML('beforeend', '<li note=' + notes.g[i] + '>' + notes.g[i] + '</li>');
+    document.getElementById('mask_b_list').insertAdjacentHTML('beforeend', '<li note=' + notes.b[i] + '>' + notes.b[i] + '</li>');
+    document.getElementById('mask_high_e_list').insertAdjacentHTML('beforeend', '<li note=' + notes.e[i] + '>' + notes.e[i] + '</li>');
+};
 
 docReady( () => {
     console.log("DocReady");
     
     document.querySelector('.controls a.down').addEventListener('click', () => {
-        if (!canClick) { return false; }
+        if (!canClick) { return false; };
         canClick = false;
         
-        const mask = document.getElementsByClassName('mask')
+        const mask = document.getElementsByClassName('mask');
         Array.from(mask).forEach(el => {
             let nextNote = el.querySelector('li:nth-child(12)').textContent;
 
@@ -39,22 +39,22 @@ docReady( () => {
                 el.querySelector('ul').insertAdjacentHTML('afterbegin', '<li note=' + nextNote + '>' + nextNote + '</li>');
                 el.querySelector('li:last-child').remove();
                 el.style.right = -189;
-            }, slideSpeed + 20)
+            }, slideSpeed + 20);
         });
 
         setTimeout(() => {
             changeOpenNotes();
             showNotes(noteToShow);
             canClick = true;
-        }, slideSpeed + 20)
+        }, slideSpeed + 20);
 
         return false;
     });
 
     document.querySelector('.controls a.up').addEventListener('click', () => {
-        if (!canClick) { return false; }
+        if (!canClick) { return false; };
         canClick = false;
-        const mask = document.getElementsByClassName('mask')
+        const mask = document.getElementsByClassName('mask');
         
         Array.from(mask).forEach(el => {
             let nextNote = el.querySelector('li:nth-child(2)').textContent;
@@ -71,7 +71,7 @@ docReady( () => {
 
         setTimeout(function () {
             canClick = true;
-        }, slideSpeed + 20)
+        }, slideSpeed + 20);
         return false;
     });
 
@@ -82,11 +82,12 @@ docReady( () => {
             showNotes(noteToShow);
             
             if(!event.target.classList.contains('active')) {
-                if(currentChord) currentChord.classList.remove('active')
-                if(currentModifier) currentModifier.classList.remove('active')
-                event.target.classList.toggle('active')
-                if (currentNote) currentNote.classList.toggle('active')
-            }
+                if(currentChord) currentChord.classList.remove('active');
+                if(currentModifier) currentModifier.classList.remove('active');
+                event.target.classList.toggle('active');
+                if (currentNote) currentNote.classList.toggle('active');
+            };
+
             currentNote = event.target;
             chordToShow = null;
         });
@@ -102,7 +103,7 @@ docReady( () => {
                 if(currentChord && currentChord !== event.target) {
                     currentChord.classList.remove('active');
                 }
-                if(currentNote.classList.contains('active')) currentNote.classList.toggle('active')
+                if(currentNote.classList.contains('active')) currentNote.classList.toggle('active');
                 currentChord = event.target;
             }
         });
@@ -119,7 +120,7 @@ docReady( () => {
                     event.target.classList.toggle('active');
                     if (currentModifier && currentModifier !== event.target) {
                         currentModifier.classList.remove('active');
-                    }
+                    };
                     currentModifier = event.target;
                 }
             }
@@ -139,14 +140,14 @@ function docReady(fn) {
 
 function showNotes(noteToShow) {
     if (noteToShow == "All") {
-        let all = document.querySelectorAll('.guitar-neck .notes li')
+        let all = document.querySelectorAll('.guitar-neck .notes li');
         all.forEach( el => {
             //el.animate({ opacity: 1 }, 500);
-            el.style.opacity = 1
-        })
+            el.style.opacity = 1;
+        });
 
     } else {
-        let notesToRemove = document.querySelectorAll('.guitar-neck .notes li:not([note="'+ noteToShow +'"])')
+        let notesToRemove = document.querySelectorAll('.guitar-neck .notes li:not([note="'+ noteToShow +'"])');
         notesToRemove.forEach(el => {
             //el.animate({ opacity: 0 }, 500);
             el.style.opacity = 0;
@@ -161,8 +162,8 @@ function showNotes(noteToShow) {
 
 function showChord(chordToShow, modifier) {
     if(!modifier) modifier = '5';
-    let chordNotes = chords[modifier][chordToShow]
-    let notesToShow
+    let chordNotes = chords[modifier][chordToShow];
+    let notesToShow;
     let notesToRemove = document.querySelectorAll('.guitar-neck .notes li');
     notesToRemove.forEach(el => {
         //el.animate({ opacity: 0 }, 500);
@@ -170,7 +171,7 @@ function showChord(chordToShow, modifier) {
     })
     
     chordNotes.forEach(note => {
-        notesToShow = document.querySelectorAll('.guitar-neck .notes li[note="' + note + '"]')
+        notesToShow = document.querySelectorAll('.guitar-neck .notes li[note="' + note + '"]');
         notesToShow.forEach(el => {
             //el.animate({ opacity: 1 }, 500);
             el.style.opacity = 1;
@@ -180,7 +181,7 @@ function showChord(chordToShow, modifier) {
 }
 
 function changeOpenNotes() {
-    masks = document.querySelector('.notes .mask')
+    masks = document.querySelector('.notes .mask');
     Array.from(masks).forEach(el => {
         let elClass = el.attr('class').split(' ')[1];
         let note = el.find('li:last-child').text();
